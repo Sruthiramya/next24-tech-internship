e18ee6dacee6a3acd1ad72ab272a16bdb97ca533
Sruthiramya <166802035+Sruthiramya@users.noreply.github.com>
2024-06-25 09:13:39 +0530

    Create userController.js

---
+++ userController.js
@@ -0,0 +1,80 @@
+// backend/controllers/userController.js
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Register new user
+const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        const user = await User.create({
+            name,
+            email,
+            password
+        });
+
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// User login
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                token: generateToken(user._id)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get user profile
+const getUserProfile = async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+};
+
+// Generate JWT Token
+const generateToken = (id) => {
+    return jwt.sign({ id }, 'your_jwt_secret', {
+        expiresIn: '30d'
+    });
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getUserProfile
+};
